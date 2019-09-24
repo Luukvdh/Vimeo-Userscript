e94b10e8c2ae74a8ba2c484b7a5e7aa36e322e4f
Luukvdh <postvoorluuk@hotmail.com>
2019-09-25 00:14:00 +0200

    v9

--- redactie.js
+++ redactie.js
@@ -2,7 +2,7 @@
 // @name        Redactie E-WISE Vimeo script
 // @namespace   ewise
 // @include     https://vimeo.com/manage/*
-// @version     v1.1
+// @version     1.1
 // @grant       none
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 
