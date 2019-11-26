74834f20ae3ec23c459d05e2d88e9c428177b3e2
Luukvdh <postvoorluuk@hotmail.com>
2019-11-26 19:33:52 +0100

    stage

--- snelheidscontrole.js
+++ snelheidscontrole.js
@@ -16,21 +16,25 @@
 
     var videoelem;
     var buttonelem;
+    var clickarea;
     var rate = 1.0;
     
     $(window).on('load', function() {
     
     setTimeout(function() {
     videoelem = getElementByXpath('/html/body/div[1]/div/div[2]/div/div[2]/div[1]/div[2]/div[1]/div/div/div/div[1]/div/div/div[1]/div[1]/div/video');
+
+
     
-    buttonelem = $("button span")[2];
+    buttonelem = $('span:contains("Send file")')[1];
     
     buttonelem.innerHTML = rate.toFixed(2)+"x";
     
     console.log(videoelem);
-    console.dir(buttonelem)
+    console.log(buttonelem);
+    
     document.addEventListener('keypress', function(key) {
-    key.preventDefault();
+    
         console.log(key.keyCode);
         var keycode = key.keyCode;
         if(keycode == 45) {
@@ -39,7 +43,9 @@
     buttonelem.innerHTML = rate.toFixed(2)+"x";
         };
     
-    if(keycode == 61 || keycode == 43) {
+   if ((keycode==43) &&
+        (key.location===3)) {
+
         rate = rate+0.1;
     videoelem.playbackRate = rate;
     buttonelem.innerHTML = rate.toFixed(2)+"x";
@@ -51,11 +57,16 @@
     videoelem.playbackRate = rate;
     buttonelem.innerHTML = rate.toFixed(2)+"x";
         };
-    
-    
-    
-    });
-    },1000);
+   if ((keycode==13) &&
+        (key.location===3)) {
+
+       clickarea = getElementByXpath('/html/body/div[1]/div/div[2]/div/div[2]/div[1]/div[2]/div[1]/div/div/div/div[2]/div/button');
+       simulate(clickarea, "click", { pointerX: 123, pointerY: 321 });
+    };
+
+
+
+    }); },1000);
     
     });
     
@@ -64,4 +75,65 @@
     function getElementByXpath(path) {
           return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
         }
+
+function simulate(element, eventName)
+{
+    var options = extend(defaultOptions, arguments[2] || {});
+    var oEvent, eventType = null;
+
+    for (var name in eventMatchers)
+    {
+        if (eventMatchers[name].test(eventName)) { eventType = name; break; }
+    }
+
+    if (!eventType)
+        throw new SyntaxError('Only HTMLEvents and MouseEvents interfaces are supported');
+
+    if (document.createEvent)
+    {
+        oEvent = document.createEvent(eventType);
+        if (eventType == 'HTMLEvents')
+        {
+            oEvent.initEvent(eventName, options.bubbles, options.cancelable);
+        }
+        else
+        {
+            oEvent.initMouseEvent(eventName, options.bubbles, options.cancelable, document.defaultView,
+            options.button, options.pointerX, options.pointerY, options.pointerX, options.pointerY,
+            options.ctrlKey, options.altKey, options.shiftKey, options.metaKey, options.button, element);
+        }
+        element.dispatchEvent(oEvent);
+    }
+    else
+    {
+        options.clientX = options.pointerX;
+        options.clientY = options.pointerY;
+        var evt = document.createEventObject();
+        oEvent = extend(evt, options);
+        element.fireEvent('on' + eventName, oEvent);
+    }
+    return element;
+}
+
+function extend(destination, source) {
+    for (var property in source)
+      destination[property] = source[property];
+    return destination;
+}
+
+var eventMatchers = {
+    'HTMLEvents': /^(?:load|unload|abort|error|select|change|submit|reset|focus|blur|resize|scroll)$/,
+    'MouseEvents': /^(?:click|dblclick|mouse(?:down|up|over|move|out))$/
+}
+var defaultOptions = {
+    pointerX: 100,
+    pointerY: 100,
+    button: 0,
+    ctrlKey: false,
+    altKey: false,
+    shiftKey: false,
+    metaKey: false,
+    bubbles: true,
+    cancelable: true
+}
     
