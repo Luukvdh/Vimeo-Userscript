428cede6302efb6071b40160af960da4d1729bc9
LuukvandenHoogen <46820191+LuukvandenHoogen@users.noreply.github.com>
2021-03-30 19:08:39 +0200

    Create autosaver.user.js

---
+++ autosaver.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Autosaver 2000
+// @namespace    http://www.turtlebank.nl/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://www.turtlebank.nl/turtle_designer.php?Dianr=*
+// @icon         https://www.google.com/s2/favicons?domain=turtlebank.nl
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("volgende!!");
+setTimeout(() => { volgende();}, 9000);
+    // Your code here...
+})();
