60bb2dd4d177088d0ec2266f765e3ec421f00d4c
LuukvandenHoogen <46820191+LuukvandenHoogen@users.noreply.github.com>
2021-03-30 20:23:36 +0200

    Update autosaver.user.js
    with favicon

--- autosaver.user.js
+++ autosaver.user.js
@@ -4,14 +4,15 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        http://www.turtlebank.nl/turtle_designer.php?Dianr=*
-// @icon         https://www.google.com/s2/favicons?domain=turtlebank.nl
+// @match        http://www.turtlebank.nl/turtle_designer.php*
+// @icon         http://www.turtlebank.nl/favicon.ico
 // @grant        none
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // ==/UserScript==
 
 (function() {
     'use strict';
-    console.log("volgende!!");
-setTimeout(() => { volgende();}, 9000);
-    // Your code here...
+    console.log("AUTOSAVER 2000 ACTIVE!");
+    $(document).ready(function() { autosaver = true;});
+
 })();
