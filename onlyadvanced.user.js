17cea27ccec88713131ecea57ca2c0d5a3f3ddc2
LuukvandenHoogen <46820191+LuukvandenHoogen@users.noreply.github.com>
2021-04-02 12:09:11 +0200

    exlude search

--- onlyadvanced.user.js
+++ onlyadvanced.user.js
@@ -1,12 +1,10 @@
 // ==UserScript==
 // @name         Only Advanced Please!
-// @namespace    https://vimeo.com/manage/videos/
-// @version      1.2
+// @namespace    https://vimeo.com
+// @version      1.7
 // @description  try to take over the world!
 // @author       You
 // @match        https://vimeo.com/manage/videos/*
-// @exclude      https://*.vimeo.com/manage/search/*
-// @exclude      https://vimeo.com/manage/search/*
 // @icon         https://www.google.com/s2/favicons?domain=vimeo.com
 // @grant        none
 // ==/UserScript==
@@ -14,9 +12,10 @@
 (function() {
     'use strict';
 var link = location.href;
+    if(!link.includes("search")) {
 link = link.replace("manage/videos/","manage/");
 link = link + "/general";
-location.href = link;
+location.href = link;};
 
-    
+
 })();
