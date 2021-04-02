bd537f2296c694e0bc01b2f0e55f01c1026ffa30
LuukvandenHoogen <46820191+LuukvandenHoogen@users.noreply.github.com>
2021-04-02 11:24:46 +0200

    Update onlyadvanced.user.js

--- onlyadvanced.user.js
+++ onlyadvanced.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
-// @name         Only Advanced Please!!
+// @name         Only Advanced Please!
 // @namespace    https://vimeo.com/
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://vimeo.com/manage/videos/*
+// @exclude      https://vimeo.com/manage/search/*
 // @icon         https://www.google.com/s2/favicons?domain=vimeo.com
 // @grant        none
 // ==/UserScript==
