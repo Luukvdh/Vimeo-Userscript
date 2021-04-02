ebf2ec49853a2ddc250c804a1da6447cabdac121
LuukvandenHoogen <46820191+LuukvandenHoogen@users.noreply.github.com>
2021-04-02 10:54:21 +0200

    Create onlyadvanced.user.js

---
+++ onlyadvanced.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Only Advanced Please!!
+// @namespace    https://vimeo.com/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://vimeo.com/manage/videos/*
+// @icon         https://www.google.com/s2/favicons?domain=vimeo.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+var link = location.href;
+link = link.replace("manage/videos/","manage/");
+link = link + "/general";
+location.href = link;
+
+    
+})();
