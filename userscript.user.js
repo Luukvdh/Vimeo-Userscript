8b6b03e85d7dbb1f111b724fbaa60418e31635c6
LuukvandenHoogen <46820191+LuukvandenHoogen@users.noreply.github.com>
2021-05-18 08:46:51 +0200

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -2,7 +2,7 @@
 // @name        Studio E-WISE functies (thumbnails, correcties, versiebeheer)
 // @namespace   ewise
 // @include     https://vimeo.com/manage/videos/search/*
-// @version     1.3
+// @version     1.4
 // @grant   none
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.8/FileSaver.min.js
