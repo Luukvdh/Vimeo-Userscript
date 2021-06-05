85b4cae4dc026bfd74a5e8b49cbe1400f1d6e136
LuukvandenHoogen <46820191+LuukvandenHoogen@users.noreply.github.com>
2021-06-05 21:06:32 +0200

    Update allvideosCursusButton.user.js
    elements via DOMModified stuk voor stuk, garandeert soepele load

--- allvideosCursusButton.user.js
+++ allvideosCursusButton.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         'Cursus' knopje in ALL VIDEOS
 // @namespace    http://tampermonkey.net/
-// @version      1.32
+// @version      1.4
 // @description  try to take over the world!
 // @author       You
 // @match        https://vimeo.com/manage/folders*
@@ -9,63 +9,53 @@
 // @grant        none
 // ==/UserScript==
 
-function goToWork(a) {
-    console.log(a.readyState);
-    setTimeout(function() {
+function goToWork() {
+var nr = 0;
+$('body').on("DOMSubtreeModified",function(a) {if (a.target.classList[0] == "video_manager__table_item" && a.eventPhase == 3) {var el2 = a.target; var el = $(el2).children().eq(2).children().first().children().first(); console.dir(el);
+    // $(spin).on("DOMSubtreeModified", function() {console.log("NU");
 
-$(".table_cell__title_wrapper").each(function(nr, el) {
 
-    var txt = $(el).children().first().text();
+
+
+    var txt = $(el).text();
 
     let newstr = txt.substring(0, txt.lastIndexOf("-"));
    console.log(newstr);
 
-    var $button = $('                 <a/>',{
+    var $button = $('<a/>',{
     text:  'cursus',
     href: 'https://vimeo.com/manage/videos/search/'+encodeURI(newstr),
     name:    'foundtitles',
     id: 'foundtitle'+nr,
-    style: 'padding: 6px; padding-top: 3px; padding-bottom: 3px; color: white; background-color: #19B7EA; display: block; float: right; z-index:999; margin-left:1%; border-radius: 4px;',
+    style: 'padding: 6px; padding-top: 1px; padding-bottom: 1px; padding-right: 6px; color: white; background-color: #19B7EA; display: inline; z-index:999; margin-left:18px; margin-right:8px; border-radius: 4px;',
     onmouseover: 'javascript:this.style.backgroundColor = "#0088CC";',
     onmouseout: 'javascript:this.style.backgroundColor = "#19B7EA";'
   });
 $button.appendTo(el);
+   console.dir(el.parent().parent().next());
+nr++;
+
+
+};});
 
 
 
-});
-
-
-
-    },4000);
+   
+ 
 };
 (function() {
 
 start0();
 
-
+document.addEventListener('keydown', function (e) {
+    if (e.keyCode == 119) { // F8
+        debugger;
+    }
+}, {
+    capture: true
+});
 
 })();
 
-function start0() {var readylisten = document.addEventListener('readystatechange', function() {if (document.readyState == "complete") {goToWork(this);};}, false);};
-
-// $("span").on('click', function() {console.log("hiieroooo"); $("span").unbind(); $('[name="foundtitles"]').remove(); goToWork();});
-// $("button").on('click', function() {console.log("hiieroooo"); $("button").unbind(); $('[name="foundtitles"]').remove(); goToWork();});
-
-function findTitle(input) {
-var regex = /-/g;
-
-
-var result = [];
-var match;
-while (match = regex.exec(input)) {
-   result.push(match.index); }
-var foundtitle = result[1];
- if(result[0]) {
-foundtitle = input.substring(parseInt(result[0])+1, parseInt(result[1]));};
-
-if(result[0] > 6) {
-foundtitle = input.substring((input.indexOf(" ")+1), result[0]);};
-return foundtitle;
-}
+function start0() {var readylisten = document.addEventListener('readystatechange', function() {if (document.readyState == "complete") {goToWork();};}, false);};
 
