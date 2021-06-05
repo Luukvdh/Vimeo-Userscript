df1bda62982847b113279489a75f23eade03b212
LuukvandenHoogen <46820191+LuukvandenHoogen@users.noreply.github.com>
2021-06-05 17:57:31 +0200

    Update allvideosCursusButton.user.js

--- allvideosCursusButton.user.js
+++ allvideosCursusButton.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         'Cursus' knopje in ALL VIDEOS
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://vimeo.com/manage/folders*
@@ -9,44 +9,45 @@
 // @grant        none
 // ==/UserScript==
 
-function goToWork() {
-  
-  $("span").on('click', function() {console.log("hiieroooo"); $("span").unbind(); setTimeout(function() {goToWork();},1000);});
-    //$("button").on('click', function() {console.log("hiieroooo"); $("button").unbind(); setTimeout(function() {goToWork();},1000);});
-  $("body").find("div").on('click', function() {this.unbind();goToWork();});
-  $("[name='search-input']").keypress(function (e) {
-    if(e.which ==13)
-    {};
-});
+function goToWork(a) {
+    console.log(a.readyState);
+    setTimeout(function() {
 
-var $all;
-setTimeout(function() {console.dir($('.table_cell__title_wrapper').toArray()); var $all = $('.table_cell__title_wrapper').toArray(); $('[name=foundtitles]').remove();
-$all.forEach(function (a,b) {
-var temptitle = a.textContent;
-temptitle = findTitle(temptitle);
-var target = $('.table_cell__title_wrapper')[b];
+$(".table_cell__title_wrapper").each(function(nr, el) {
 
-var $button = $('<a/>',{
+    var txt = $(el).children().first().text();
+
+    let newstr = txt.substring(0, txt.lastIndexOf("-"));
+   console.log(newstr);
+
+    var $button = $('<a/>',{
     text:  'cursus',
-    href: 'https://vimeo.com/manage/videos/search/'+temptitle,
+    href: 'https://vimeo.com/manage/videos/search/'+encodeURI(newstr),
     name:    'foundtitles',
-    id: 'foundtitle'+b,
+    id: 'foundtitle'+nr,
     style: 'padding: 6px; padding-top: 3px; padding-bottom: 3px; color: white; background-color: #19B7EA; display: block; float: right; z-index:999; margin-left:1%; border-radius: 4px; position: relative;',
     onmouseover: 'javascript:this.style.backgroundColor = "#0088CC";',
     onmouseout: 'javascript:this.style.backgroundColor = "#19B7EA";'
   });
-var $target = $(target);
-var thisurl = $(location).attr('href');
+$button.appendTo(el);
 
-if(!thisurl.includes("videos")) { 
-$button.appendTo($target).hide().fadeIn(300);}}
-)},1000);
 
+
+});
+
+
+
+    },3200);
 };
 (function() {
-  setTimeout(function() {goToWork();},200);
-$('.active').on('click',function() {goToWork();});
+
+start0();
+
+
+
 })();
+
+function start0() {var readylisten = document.addEventListener('readystatechange', function() {if (document.readyState == "complete") {goToWork(this);};}, false);};
 
 // $("span").on('click', function() {console.log("hiieroooo"); $("span").unbind(); $('[name="foundtitles"]').remove(); goToWork();});
 // $("button").on('click', function() {console.log("hiieroooo"); $("button").unbind(); $('[name="foundtitles"]').remove(); goToWork();});
