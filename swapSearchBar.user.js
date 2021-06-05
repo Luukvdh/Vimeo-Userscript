d84d106a343862a497f8ad419ea8be2d0e6a5f3b
LuukvandenHoogen <46820191+LuukvandenHoogen@users.noreply.github.com>
2021-06-06 01:13:40 +0200

    Update swapSearchBar.user.js

--- swapSearchBar.user.js
+++ swapSearchBar.user.js
@@ -2,7 +2,7 @@
 // @name        ZOEKBALK E-Wise (Vimeo)
 // @namespace   ewise
 // @include     https://vimeo.com/*
-// @version     1.3
+// @version     1.5
 // @grant       none
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 
@@ -13,9 +13,12 @@
 
 (function() {
     'use strict';
-
-    $('#topnav-search').attr('placeholder', 'Zoek hier je E-WISE cursus...');
-    document.querySelector('#topnav-search').addEventListener('keypress', function (e) {
+$("#topnav_menu_search_omnisearch").unbind();
+    $("#topnav-search_search_form").remove();
+    $("<input id='topnav2'>").appendTo($("#topnav_menu_search_omnisearch"));
+    $('#topnav2').attr('placeholder', 'Zoek hier je E-WISE cursus...');
+    $('#topnav2').css({'width':'100%','height':'50px','border-radius': '5px','padding':'5px','padding-left':'10px','font-size':'12pt'});
+    document.querySelector('#topnav2').addEventListener('keypress', function (e) {
     var key = e.which || e.keyCode;
     if (key === 13) { // 13 is enter
     e.preventDefault();
