06afb8f8519f0029e3a94d17a6fd4b16959ba538
LuukvandenHoogen <46820191+LuukvandenHoogen@users.noreply.github.com>
2021-07-27 19:19:01 +0200

    verbeterd juli 2021

--- allvideosCursusButton.user.js
+++ allvideosCursusButton.user.js
@@ -1,18 +1,28 @@
 // ==UserScript==
 // @name         'Cursus' knopje in ALL VIDEOS
 // @namespace    http://tampermonkey.net/
-// @version      1.5
+// @version      1.6
 // @description  try to take over the world!
 // @author       You
 // @match        https://vimeo.com/manage/folders*
 // @match        https://vimeo.com/manage/videos*
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @grant        none
 // ==/UserScript==
-
-function goToWork() {
 var nr = 0;
-$('body').on("DOMSubtreeModified",function(a) {if (a.target.classList[0] == "video_manager__table_item" && a.eventPhase == 3) {var el2 = a.target; var el = $(el2).children().eq(2).children().first().children().first(); console.dir(el);
-    // $(spin).on("DOMSubtreeModified", function() {console.log("NU");
+function goToWork() {
+nr = 0;
+    $('.foundtitlesbutton').remove();
+    console.log('features clicked!');
+
+$('.video_manager__table_item').each(function(b,a)  {addCursusButton(a);  });
+
+};
+
+
+    function addCursusButton(a) {
+        var el = $(a).children().eq(2).children().first().children().first();
+
 
 
 
@@ -20,32 +30,38 @@ $('body').on("DOMSubtreeModified",function(a) {if (a.target.classList[0] == "vid
     var txt = $(el).text();
 
     let newstr = txt.substring(0, txt.lastIndexOf("-"));
-   console.log(newstr);
+
 
     var $button = $('<a/>',{
     text:  'cursus',
     href: 'https://vimeo.com/manage/videos/search/'+encodeURI(newstr)+"?",
     name:    'foundtitles',
+        class: 'foundtitlesbutton',
     id: 'foundtitle'+nr,
     style: 'padding: 6px; padding-top: 1px; padding-bottom: 1px; padding-right: 6px; color: white; background-color: #19B7EA; display: inline; z-index:999; margin-left:18px; margin-right:8px; border-radius: 4px;',
     onmouseover: 'javascript:this.style.backgroundColor = "#0088CC";',
     onmouseout: 'javascript:this.style.backgroundColor = "#19B7EA";'
   });
 $button.appendTo(el);
-   console.dir(el.parent().parent().next());
+
 nr++;
 
 
-};});
 
 
 
-   
- 
+
+
+
 };
-(function() {
+(function() {setTimeout(function() {var $sectie = $("section")[4]; $($sectie).on("DOMSubtreeModified", function(b) {var $bt = $(b.target); if($bt.hasClass('video_manager__table_item')) {addCursusButton($bt);}; });},500);
+var featuresbutton = $("[aria-owns='topnav_features']")[0]; $(featuresbutton).text("\n                                Cursusknopjes                                                                    \n                                        \n                                            \n                                        \n                                    \n                                                            ");
+     $(featuresbutton).unbind();
+    $(featuresbutton).removeAttr("onclick");
+    $(featuresbutton).on("click",function() {goToWork();});
+// start00();
 
-start0();
+
 
 document.addEventListener('keydown', function (e) {
     if (e.keyCode == 119) { // F8
@@ -57,5 +73,5 @@ document.addEventListener('keydown', function (e) {
 
 })();
 
-function start0() {var readylisten = document.addEventListener('readystatechange', function() {if (document.readyState == "complete" && location.href.indexOf("?") < 0) {goToWork();} else {$("h2").last().html(decodeURI(location.href.substring(location.href.lastIndexOf("/")+1,location.href.length-1))); };}, false);};
+function start00() {var readylisten = document.addEventListener('readystatechange', function() {if (document.readyState == "complete" && location.href.indexOf("?") < 0) {goToWork();} else {$("h2").last().html(decodeURI(location.href.substring(location.href.lastIndexOf("/")+1,location.href.length-1))); };}, false);};
 
